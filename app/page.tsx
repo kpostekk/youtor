a156442067f135cce0bbbc2e8bbb3bc4fd28fc4a
Krystian Postek <krystian@postek.eu>
2024-04-21 13:27:14 +0200

    also remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ import Flashcards from "../images/Flashcards.png"
 import Link from "next/link"
 
 export default async function Index() {
-  const supabase = createClient()
-
   return (
     <div className="grid grid-cols-2 place-content-center h-screen">
       <div className="flex justify-end">
