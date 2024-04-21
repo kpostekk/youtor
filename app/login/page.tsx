f304710a4430d59f3bcaa491156c979a31337a1f
Krystian Postek <krystian@postek.eu>
2024-04-21 13:19:25 +0200

    fix: solve login issues

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,29 +1,42 @@
-import Link from "next/link"
+// import Link from "next/link"
 import { headers } from "next/headers"
 import { createClient } from "@/utils/supabase/server"
 import { redirect } from "next/navigation"
-import { SubmitButton } from "./submit-button"
-import { defaultUrl } from "@/utils/url"
+// import { SubmitButton } from "./submit-button"
+// import { defaultUrl } from "@/utils/url"
 
 export default async function Login() {
-  const supabase = createClient()
+  const signIn = async () => {
+    'use server'
 
-  const { data } = await supabase.auth.signInWithOAuth({
-    provider: "discord",
-    options: {
-      scopes: "identify email",
-      redirectTo: `${defaultUrl()}/auth/callback`,
-    },
-  })
+    const supabase = createClient()
+    const origin = headers().get("origin")
+
+    const { data } = await supabase.auth.signInWithOAuth({
+      provider: 'discord',
+      options: {
+        scopes: "identify email",
+        redirectTo: `${origin}/auth/callback`,
+      },
+    })
+
+    if (data?.url) {
+      redirect(data.url)
+    }
+
+    throw new Error("Failed to sign in")
+  }
 
   return (
     <div className="h-screen grid place-items-center">
-      <Link
-        href={data.url!}
-        className="border bg-emerald-900 border-emerald-500 rounded-xl py-2 px-4 font-medium"
-      >
-        Sign in with Discord
-      </Link>
+      <form>
+        <button
+          formAction={signIn}
+          className="border bg-emerald-900 border-emerald-500 rounded-xl py-2 px-4 font-medium"
+        >
+          Sign in with Discord
+        </button>
+      </form>
     </div>
   )
 }
